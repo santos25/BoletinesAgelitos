bcb9dc293365916aa7880ae078e1f87f0b94fd02
DCA26\LENOVO <asantos@dcatech.com>
2017-08-01 17:25:21 -0500

    RowTable

--- src/components/RowTable.js
+++ src/components/RowTable.js
@@ -7,67 +7,89 @@ class RowTable extends Component {
     super();
     this.state = {
       desempeno : '',
+      planillas : {}
       // estudiantes : {}
     }
   }
 
-  onChangeNotas(e){
-    //Se repite en todos los campos de la tabla
-    let nota = e.target.value;
-     if(nota => 1 && nota <= 5){
-       if(nota > 4){
-            this.setState({
-              desempeno : 'Superior'
-            })
-        }else if (nota > 3 && nota <= 4) {
-          this.setState({
-            desempeno : 'Alto'
-          })
-        }
-     }
+componentWillMount(){
+  // this.ref = Base.syncState(`planillas`, {
+  //   context: this,
+  //   state: 'planillas',
+  // });
+
+}
+  changeDesempeno(desempeno){
+    this.setState({
+      desempeno : desempeno
+    })
+  }
+
+
+  onChangeRowStudent(e){
+
+    let name = e.target.name;
+    console.log(name);
+    // if(nota <= 5){
+    //   if(nota >= 4.6){
+    //     this.changeDesempeno('S');
+    //   }else if (nota >= 4.0) {
+    //     this.changeDesempeno('A');
+    //   }else if (nota >= 3.0) {
+    //     this.changeDesempeno('B');
+    //   }else {
+    //     this.changeDesempeno('B.J');
+    //   }
+    // }
   }
 
 
   render(){
-        // console.log(props);
-          return(
-            // null
-            <tr>
-              <td>{this.props.estudiante.nombre + " " +
-                this.props.estudiante.apellido}
-              </td>
-              <td>
-                <FormGroup controlId="formControlsTextarea">
-                  <FormControl componentClass="textarea" placeholder="textarea" />
-                </FormGroup>
-              </td>
-              <td>
-                <FormGroup controlId="formControlsText">
-                  <FormControl  id="formControlsText"
-                    onChange={this.onChangeNotas.bind(this)}
-                    type="text"
-                    placeholder="Enter Nota"/>
-                  </FormGroup>
-                </td>
-                <td>
-                  <FormGroup controlId="formControlsText">
-                    <FormControl disabled="true"  id="formControlsText"
-                      type="text"
-                      value={this.state.desempeno}
-                    />
-                  </FormGroup>
-                </td>
-                <td>
-                  <FormGroup controlId="formControlsText">
-                    <FormControl width="25px"  id="formControlsText"
-                      type="text"
-                      placeholder="Horas"/>
-                    </FormGroup>
-                  </td>
-                </tr>
-          )
+    // console.log(props);
+    return(
+      // null
+      <tr>
+        <td>{this.props.estudiante.nombre + " " +
+          this.props.estudiante.apellido}
+        </td>
+        <td>
+          <FormGroup controlId="formControlsTextarea">
+            <FormControl  onChange={this.onChangeRowStudent.bind(this)}
+                      componentClass="textarea"
+                      name="descripcion"
+                      placeholder="textarea" />
+          </FormGroup>
+        </td>
+        <td>
+          <FormGroup controlId="formControlsText">
+            <FormControl  id="formControlsText"
+              onChange={this.onChangeRowStudent.bind(this)}
+              type="text"
+              name="nota"
+              placeholder="Enter Nota"/>
+            </FormGroup>
+          </td>
+          <td>
+            <FormGroup controlId="formControlsText">
+              <FormControl disabled="true"  id="formControlsText"
+                type="text"
+                value={this.state.desempeno}
+              />
+            </FormGroup>
+          </td>
+          <td>
+            <FormGroup controlId="formControlsText">
+              <FormControl width="25px"  id="formControlsText"
+                type="text"
+                name="horas"
+                onChange={this.onChangeRowStudent.bind(this)}
+                placeholder="Horas"/>
+              </FormGroup>
+            </td>
+          </tr>
+        )
       }
     }
 
 
-      export default RowTable ;
+    export default RowTable ;
