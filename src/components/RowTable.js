6bc8caac60add3cacfd58480ad0852fed92021bd
DCA26\LENOVO <asantos@dcatech.com>
2017-08-04 14:33:11 -0500

    Falta Corregir Rowtable

--- src/components/RowTable.js
+++ src/components/RowTable.js
@@ -24,42 +24,34 @@ class RowTable extends Component {
   changeDataRow(value, name,keyEstudiante, desempeno){
     // estudiante[name] = value;
     // estudiante['desempeno'] = desempeno
-
-    let estuEdit = {...this.state.estudiantes[keyEstudiante], [name] : value}
-    let estudiantes = this.state.estudiantes[keyEstudiante] = estuEdit;
+    let estudiantes = {...this.state.estudiantes};
+    let estuEdit = {...estudiantes[keyEstudiante], [name] : value};
+    estudiantes[keyEstudiante] = estuEdit;
+    // console.log(estudiantes);
     this.setState({
       estudiantes
     })
-    // this.setState((prevState) =>{
-    //   desempenoChange : !prevState.desempenoChange
-    // })
+
   }
 
   onChangeRowStudent(e, keyEstudiante){
 
     let name = e.target.name;
-    // let estudiante = this.props.estudiantes[keyEstudiante];
     if(name === 'nota')  {
       if(e.target.value <= 5){
         if(e.target.value >= 4.6){
-          this.changeDataRow(e.target.value, name,'S');
+          this.changeDataRow(e.target.value, name, keyEstudiante,'S');
         }else if (e.target.value >= 4.0) {
-          this.changeDataRow(e.target.value, name,'A');
+          this.changeDataRow(e.target.value, name, keyEstudiante,'A');
         }else if (e.target.value >= 3.0) {
-          this.changeDataRow(e.target.value, name,'B');
+          this.changeDataRow(e.target.value, name, keyEstudiante,'B');
         }else {
-          this.changeDataRow(e.target.value, name,'B.J');
+          this.changeDataRow(e.target.value, name, keyEstudiante,'B.J');
         }
       }
     }else {
       this.changeDataRow(e.target.value , name ,keyEstudiante)
-      // let estuEdit = {...this.state.estudiantes[keyEstudiante], [name] : e.target.value}
-      // let estudiantes = this.state.estudiantes[keyEstudiante] = estuEdit;
-      // this.setState({
-      //   estudiantes
-      // })
     }
-
   }
 
 
@@ -71,8 +63,12 @@ class RowTable extends Component {
         {
           Object.keys(this.props.estudiantes)
           .map((keyEstudiante) =>{
-            console.log(this.state.estudiantes[keyEstudiante].descripcion);
-            // let descripcion = this.state.estudiantes[keyEstudiante].descripcion
+            // console.log(this.state.estudiantes[keyEstudiante].descripcion);
+            let descripcion = this.state.estudiantes[keyEstudiante].descripcion;
+            // let nota = this.state.estudiantes[keyEstudiante].nota;
+            // let desempeno = this.state.estudiantes[keyEstudiante].desempeno;
+            // let horas = this.state.estudiantes[keyEstudiante].horas;
+
             return(
               <tr key={keyEstudiante}>
                 <td>{this.props.estudiantes[keyEstudiante].nombre + " " +
@@ -83,43 +79,49 @@ class RowTable extends Component {
                     <FormControl  onChange={(e) => this.onChangeRowStudent(e, keyEstudiante)}
                       componentClass="textarea"
                       name="descripcion"
-                      // value={descripcion}
+                      value={descripcion}
                       placeholder="Descripcion" />
                     </FormGroup>
                   </td>
                   <td>
-                    <FormGroup controlId="formControlsText">
-                      <FormControl  id="formControlsText"
-                        onChange={(e) => this.onChangeRowStudent(e, keyEstudiante)}
-                        type="text"
-                        name="nota"
-                        // value={this.state.estudiantes[keyEstudiante].nota}
-                        placeholder="Enter Nota"/>
-                      </FormGroup>
-                    </td>
-                    <td>
-                        <h3 className="formPlanilla">
-                          {/* <Label bsStyle="info">{this.props.estudiantes[keyEstudiante].desempeno}</Label> */}
-                        </h3>
-                    </td>
-                    <td>
-                      <FormGroup controlId="formControlsText">
-                        <FormControl width="25px"  id="formControlsText"
-                          type="text"
-                          name="horas"
-                          // value={this.state.estudiantes[keyEstudiante].horas}
-                          onChange={(e) => this.onChangeRowStudent(e, keyEstudiante)}
-                          placeholder="Horas"/>
-                        </FormGroup>
-                      </td>
-                    </tr>
-                  )
-                })
-              }
-            </tbody>
-          )
-        }
-      }
+                    <input name="nota"
+                      type="text"
+                      value={descripcion}
+                      onChange={(e) => this.onChangeRowStudent(e, keyEstudiante)}
+                    />
+                    {/* <FormGroup controlId="formControlsText">
+                    <FormControl  id="formControlsText"
+                    onChange={(e) => this.onChangeRowStudent(e, keyEstudiante)}
+                    type="text"
+                    name="nota"
+                    value={descripcion}
+                    placeholder="Enter Nota"/>
+                  </FormGroup> */}
+                </td>
+                <td>
+                  <h3 className="formPlanilla">
+                    {/* <Label bsStyle="info">{desempeno}</Label> */}
+                  </h3>
+                </td>
+                <td>
+                  <FormGroup controlId="formControlsText">
+                    <FormControl
+                      id="formControlsText"
+                      type="text"
+                      name="horas"
+                      // value={this.state.estudiantes[keyEstudiante].horas}
+                      onChange={(e) => this.onChangeRowStudent(e, keyEstudiante)}
+                      placeholder="Horas"/>
+                    </FormGroup>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      )
+    }
+  }
 
 
-      export default RowTable ;
+  export default RowTable ;
