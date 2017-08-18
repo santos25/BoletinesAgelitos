77b0fb5893b6aed5efc18806def306092139cf2d
DCA26\LENOVO <asantos@dcatech.com>
2017-08-18 16:28:18 -0500

    Diseñando Boletin

--- src/components/boletines/PreviewBoletin.js
+++ src/components/boletines/PreviewBoletin.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Grid,Row,Col,Table,Label} from 'react-bootstrap';
 import HeaderTable from '../planilla/HeaderTable';
+import escudo from '../escudo.jpg'
 // import HeaderTable from '../planilla/HeaderTable';
 // import RowTableBoletin from './RowTableBoletin';
 
@@ -10,34 +11,79 @@ const PreviewBoletin = (props) => {
   const {planilla , asignaturas , keyEstudiante} = props;
   return(
     <div>
-      <div className="formPlanilla">
-        <h4><Label bsStyle="default">INFORME DEL DESEMPEÑO DEL ESTUDIANTE</Label></h4>
-      </div>
-      <Table striped bordered condensed hover>
-        <HeaderTable  columns={columns}/>
-        <tbody>
-          {Object.keys(asignaturas).map ((key, i) => {
-            if (planilla[0][key] && keyEstudiante !== '') {
-              let estudianteData = planilla[0][key].estudiantes[keyEstudiante]
-              console.log(estudianteData);
-              return(
-                <tr key={key}>
-                  <td>{planilla[0][key].nombre}
+      <Row className="show-grid">
+        <Col xs={12} md={4}>
+          <img className="escudo" src={escudo} alt="Escudo"></img>
+        </Col>
+        <Col xs={12} md={4}>
+          <div>
+            <Label>INSTITUTO ANGELITOS ALEGRES </Label>
+          </div>
+          <div>
+            <Label>Nuevo Bosque Trv. 51    N° 29 B 77</Label>
+          </div>
+          <div>
+            <Label>Aprobado por Resolución N° 7962</Label>
+          </div>
+          <div>
+            <Label>Dane: 313001000185</Label>
+          </div>
+        </Col>
+        <Col xs={12} md={4}>
+          <div>
+            <Label>Andres Fernado Santos Gueto</Label>
+          </div>
+        </Col>
+      </Row>
+      <Row className="show-grid">
+        <Col xs={12} md={12}>
+          <div className="tittleBoletinContainer">
+            <div className="tittleFloating">
+              <Label bsStyle="default">INFORME ACADEMICO </Label>
+            </div>
+            <div className="tittleFloating">
+              <Label bsStyle="default">{`GRADO : ${planilla[0].grado}`} </Label>
+            </div>
+            <div className="tittleFloating">
+              <Label bsStyle="default">JORNADA : Mañana</Label>
+            </div>
+            <div className="tittleFloating">
+              <Label bsStyle="default">{`PERIODO : ${planilla[0].periodo}`}</Label>
+            </div>
+            <div className="tittleFloating">
+              <Label bsStyle="default"> {`AÑO : ${ new Date().getFullYear()}`}</Label>
+            </div>
+          </div>
+        </Col>
+
+      </Row>
+      <Row>
+        <Table striped bordered condensed hover>
+          <HeaderTable  columns={columns}/>
+          <tbody>
+            {Object.keys(asignaturas).map ((key, i) => {
+              if (planilla[0][key] && keyEstudiante !== '') {
+                let estudianteData = planilla[0][key].estudiantes[keyEstudiante]
+                console.log(estudianteData);
+                return(
+                  <tr key={key}>
+                    <td>{planilla[0][key].nombre}
+                    </td>
+                    <td><pre>{estudianteData.descripcion}</pre>
                   </td>
-                  <td><pre>{estudianteData.descripcion}</pre>
-                </td>
-                <td>{estudianteData.nota}
-                </td>
-                <td>{estudianteData.desempeno}
-                </td>
-                <td>{estudianteData.horas}
-                </td>
-              </tr>
-            )
-          }
-        })}
-      </tbody>
-    </Table>
+                  <td>{estudianteData.nota}
+                  </td>
+                  <td>{estudianteData.desempeno}
+                  </td>
+                  <td>{estudianteData.horas}
+                  </td>
+                </tr>
+              )
+            }
+          })}
+        </tbody>
+      </Table>
+    </Row>
   </div>
 )
 }
