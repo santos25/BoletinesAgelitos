dcba85fad999f51c72d74f7846dc7d2744c6dc40
Andres Santos <santfer25@hotmail.com>
2020-08-24 14:08:09 -0500

    Cambios periodo 2 2020

--- src/components/boletines/DataBoletines.js
+++ src/components/boletines/DataBoletines.js
@@ -73,7 +73,7 @@ class DataBoletines extends Component {
         console.log(data);
         for (var x = 0; x < data.length; x++) {
           //console.log(data[x].ano);
-            if (data[x].ano === 2020 && data[x].periodo === 1) {
+            if (data[x].ano === 2020 && data[x].periodo === 2) {
                 console.log(data[x]);
                 var newVector = new Array();
                 newVector[0] = data[x];
