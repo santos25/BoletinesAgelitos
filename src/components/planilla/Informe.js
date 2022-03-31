444ee928ecf5a2b56ddcb9ee2d996bb2773931bc
Andres Fernando Santos Gueto <andres.santos.gueto@ey.com>
2022-03-31 15:50:42 -0500

    periodo 1 año 2022

--- src/components/planilla/Informe.js
+++ src/components/planilla/Informe.js
@@ -16,12 +16,12 @@ class Informe extends Component {
 
   showPlanilla(e) {
     e.preventDefault();
-    if (this.state.keyPeriodoSelected === 'periodo4') {
+    if (this.state.keyPeriodoSelected === 'periodo1') {
       this.setState((prevState) => ({
         isPlanilla: !prevState.isPlanilla,
       }));
     } else {
-      Alert.error('Seleccione Periodo 4!!', {
+      Alert.error('Seleccione Periodo 1!!', {
         position: 'bottom-left',
         effect: 'scale',
 
