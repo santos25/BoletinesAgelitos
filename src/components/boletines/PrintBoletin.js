279e59f24eecbc1a62aab88bf5f000d7f9ba09ab
Andres Fernando Santos Gueto <andres.santos.gueto@ey.com>
2022-04-24 20:36:40 -0500

    boletines primer periodo

--- src/components/boletines/PrintBoletin.js
+++ src/components/boletines/PrintBoletin.js
@@ -77,12 +77,12 @@ const PrintBoletin = (props) => {
           </div>
           <div className="periodo">
             <p className="styleletterPrimary">Periodo </p>
-            <p className="styleletterPrimary">2</p>
+            <p className="styleletterPrimary">1</p>
             {/* <Label bsStyle="default">{`PERIODO : ${planilla[0].periodo}`}</Label>*/}
           </div>
           <div className="año">
             <p className="styleletterPrimary">Año </p>
-            <p className="styleletterPrimary">2021 </p>
+            <p className="styleletterPrimary">2022 </p>
           </div>
         </div>
         <div>
